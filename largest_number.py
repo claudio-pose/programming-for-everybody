1fc642567004f7bc7c72bf3965edae5b965ade28
Claudio Pose <claudio@pose.ch>
2017-02-19 16:46:10 +0100

    Assignment Chapter 4

---
+++ largest_number.py
@@ -0,0 +1,24 @@
+largest = None
+smallest = None
+
+while True:
+    num = raw_input("Enter a number: ")
+    if num == "done" : break
+
+    try:
+        num = int(num)
+
+        if smallest is None:
+            smallest = num
+        elif num < smallest:
+            smallest = num
+
+        if largest is None:
+            largest = num
+        elif num > largest:
+            largest = num
+    except ValueError:
+        print 'Invalid input'
+
+print "Maximum", largest
+print "Minimum", smallest
