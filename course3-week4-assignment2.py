fb629b5c9457259b65fe67bb7d240bf108ef5fb3
Claudio Pose <claudio@pose.ch>
2017-02-26 21:44:39 +0100

    Assignment completed

--- course3-week4-assignment2.py
+++ course3-week4-assignment2.py
@@ -4,18 +4,16 @@ from BeautifulSoup import *
 # Load page from server
 def loadURL(url, position, steps):
     print 'Retrieve ' + url + '...'
-    html = urllib.urlopen(url).read()
-    # Parse HTML
-    soup = BeautifulSoup(html)
-    # Retrieve all of the anchor tags
-
-    tags = soup('a')
-    tag = tags[position]
-    key, url = tag.attrs[0]
 
     steps = steps - 1
     if steps >= 0:
-        loadURL(url, position, steps)
+        html = urllib.urlopen(url).read()
+        # Parse HTML
+        soup = BeautifulSoup(html)
+        # Retrieve all of the anchor tags
+
+        tags = soup('a')
+        loadURL(tags[position].get('href', None), position, steps)
 
 position = int(raw_input('Position:')) - 1
 steps = int(raw_input('Steps:'))
